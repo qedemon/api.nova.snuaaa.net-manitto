9a8e6f729f53786e8932177ba8981472cc7fc075
qedemon <qedemon@snu.ac.kr>
2024-01-24 00:21:17 +0000

    midlayer file create

--- modules/policy/module.js
+++ modules/policy/module.js
@@ -1,7 +1,9 @@
 const getPolicies = require("./getPolicies");
-const setPolicy = require("./setPolicy")
+const setPolicy = require("./setPolicy");
+const midLayer = require("./midLayer");
 
 module.exports = {
     getPolicies,
-    setPolicy
+    setPolicy,
+    midLayer
 }
