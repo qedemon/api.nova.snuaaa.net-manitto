734c144efd7f75c9cba28e08b10549dc4175c5ce
qedemon <qedemon@snu.ac.kr>
2024-01-28 13:41:22 +0000

    getUser isValid 추가

--- modules/user/getUser.js
+++ modules/user/getUser.js
@@ -48,8 +48,8 @@ async function getUser(condition, loadedSequelize=null){
                             major,
                             isAdmin,
                             following: Following.map(
-                                ({Followee: {name, col_no, major}, expired_at})=>{
-                                    return {name, col_no, major, expired_at};
+                                ({Followee: {name, col_no, major}, expired_at, isValid})=>{
+                                    return {name, col_no, major, expired_at, isValid};
                                 }
                             ),
                             Schedule,
