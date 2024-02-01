258ec41d9791c1ffdfd9d447130adb465a6aa1cb
qedemon <qedemon@snu.ac.kr>
2024-02-01 16:30:53 +0000

    create

---
+++ modules/Utility/getToday.js
@@ -0,0 +1,22 @@
+const getNow = require("./getNow");
+const {convertDateToUnit, reference} = require("./convertDate");
+
+function getToday(){
+    const unit = convertDateToUnit(getNow());
+    if(unit.minor >= reference[unit.major].length-1){
+        return unit.major;
+    }
+    return (
+        (value, min, max)=>{
+            if(value<min){
+                return min;
+            }
+            if(value>max){
+                return max;
+            }
+            return value;
+        }
+    )(unit.major-1, 0, reference.length-1)
+}
+
+module.exports = getToday();
