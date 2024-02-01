c29785a9ba3595c43a277e260f1387581c1b495c
qedemon <qedemon@snu.ac.kr>
2024-02-01 18:14:24 +0000

    push 잘 되네

--- modules/connection/setConnectionDocument.js
+++ modules/connection/setConnectionDocument.js
@@ -6,6 +6,7 @@ const getToday = require("modules/Utility/getToday");
 const getConnectionDocument = require("./getConnectionDocument");
 const connect = require("./connect");
 const disconnect = require("./disconnect");
+const {sendPush} = require("modules/push/module");
 
 function getUpdate(prevData, data){
     const newConnections = data.connectionGroups.reduce(
@@ -79,27 +80,81 @@ async function setConnectionDocument(day, data, loadedSequelize=null, today=getT
         )();
         
         const updates = getUpdate(prevData, data);
-        await updates.toDisconnect.reduce(
+        const disconnected = await updates.toDisconnect.reduce(
             async (last, toDisconnect)=>{
-                await last;
-                const {error} = await disconnect(toDisconnect.follower_id, toDisconnect.followee_id, sequelize, (day>today)?{willBeValid: day}:{});
+                const result = await last;
+                const {error, disconnected} = await disconnect(toDisconnect.follower_id, toDisconnect.followee_id, sequelize, (day>today)?{willBeValid: day}:{});
+                if(error){
+                   return result; 
+                }
+                return [...result, ...disconnected]
             },
-            Promise.resolve()
+            Promise.resolve([])
         );
-        await updates.toConnect.reduce(
+        const {connected, disconnected: additionalDisconnected} = await updates.toConnect.reduce(
             async (last, toConnect)=>{
-                await last;
-                if(toConnect.followee_id)
-                    await connect(toConnect.follower_id, toConnect.followee_id, sequelize, (day>today)?{willBeValid: day}:{});
-                else
-                    await disconnect(toConnect.follower_id, null, sequelize, (day>today)?{willBeValid: day}:{});
+                const result = await last;
+                if(toConnect.followee_id){
+                    const {error, connected, disconnected} = await connect(toConnect.follower_id, toConnect.followee_id, sequelize, (day>today)?{willBeValid: day}:{});
+                    if(error){
+                        return result;
+                    }
+                    return {
+                        connected: [...result.connected, ...connected],
+                        disconnected: [...result.disconnected, ...disconnected]
+                    };
+                }
+                else{
+                    const {error, disconnected} = await disconnect(toConnect.follower_id, null, sequelize, (day>today)?{willBeValid: day}:{});
+                    if(error){
+                        return result;
+                    }
+                    return {
+                        connected: result.connected,
+                        disconnected: [...result.disconnected, ...disconnected]
+                    }
+                }
             },
-            Promise.resolve()
+            Promise.resolve({connected:[], disconnected:[]})
         );
+
+        console.log(`day${day}`, "disconnected", [...disconnected, ...additionalDisconnected]);
+        console.log(`day${day}`, "connected", connected);
+        if(day === today){
+            const pushList = [...(new Set([...disconnected, ...additionalDisconnected, ...connected].map(({follower_id})=>follower_id)))];
+            console.log(`day ${day} pushList`, pushList);
+            const pushed = (
+                await Promise.all(
+                    pushList.map(
+                        async (user_id)=>{
+                            return await sendPush(user_id, 
+                                {
+                                    title: "AAA-Manitto",
+                                    body: "마니또가 바뀌었어요"
+                                }
+                            );
+                        }
+                    )
+                )
+            ).reduce(
+                (result, {error, pushed})=>{
+                    if(error){
+                        return result;
+                    }
+                    return [...result, ...pushed];
+                },
+                []
+            );
+            console.log("pushlist: :", pushList.length, ", pushed: ", pushed.length);
+        }
+
         
         return {
             day, 
-            updates,
+            updates: {
+                disconnected: [...disconnected, ...additionalDisconnected].map(({follower_id, followee_id})=>{return {follower_id, followee_id}}),
+                connected: connected.map(({follower_id, followee_id})=>{return {follower_id, followee_id}})
+            },
             data: await (
                 async ()=>{
                     const {data, error} = await getConnectionDocument(day, sequelize, today);
