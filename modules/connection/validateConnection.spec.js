bb28aa229d0405f501e19cb8acc42fdc5db6dc53
qedemon <qedemon@snu.ac.kr>
2024-02-09 00:31:20 +0000

    autoconnect

--- modules/connection/validateConnection.spec.js
+++
@@ -1,9 +0,0 @@
-const validateConnection = require("./validateConnection");
-
-test("validateConnection", async()=>{
-    const {error, toBeConnected} = await validateConnection(null);
-    console.log(toBeConnected);
-    if(error){
-        console.error(error);
-    }
-})
