09f4a475e8ba0ecdb8d04dff2e6f375f53cc5b4f
qedemon <qedemon@snu.ac.kr>
2024-02-11 00:01:24 +0000

    setSchedule

--- modules/user/setSchedule.js
+++ modules/user/setSchedule.js
@@ -1,30 +1,12 @@
-const {createSequelize, closeSequelize} = require("modules/sequelize");
-const defineModels = require("models");
+const {connect} = require("modules/mongoose");
+const {User} = require("models/mongoDB");
 
-async function setSchedule(user_id, schedule, loadedSequelize=null){
-    const sequelize = (loadedSequelize || (await createSequelize()).sequelize);
+async function setSchedule(user_id, schedule){
     try{
-        const {User, Schedule} = defineModels(sequelize, sequelize.Sequelize.DataTypes);
-        const user = await User.findOne(
-            {
-                where: {
-                    user_id
-                },
-                include: Schedule
-            }
-        );
+        const user = await User.findById(user_id);
         if(!user){
             throw new Error(`no user with user_id ${user_id}`)
         }
-        const newScheduleInfo = {
-            enter_at: schedule?.enter_at||user.Schedule?.enter_at,
-            exit_at: schedule?.exit_at||user.Schedule?.exit_at
-        };
-        const newSchedule = await Schedule.create(
-            newScheduleInfo
-        );
-        await user.setSchedule(newSchedule);
-        await user.reload();
         return {
             user:
             (
@@ -36,18 +18,18 @@ async function setSchedule(user_id, schedule, loadedSequelize=null){
                         }
                     }
                 }
-            )(user)
+            )(await user.set({
+                schedule: {
+                    ...user.schedule,
+                    ...schedule
+                }
+            }).save())
         };
     }
     catch(error){
         return {
             error
         };
-    }
-    finally{
-        if(loadedSequelize===null){
-            closeSequelize(sequelize);
-        }
     }
 }
 
