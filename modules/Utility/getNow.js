2c6b6b659ae4765dbfcca4b4fa71d0b8f275a09c
qedemon <qedemon@snu.ac.kr>
2024-08-15 11:01:14 +0000

    now

--- modules/Utility/getNow.js
+++ modules/Utility/getNow.js
@@ -1,6 +1,7 @@
 function getNow(){
-    //return Date.now();
-    return new Date("2024-08-16T19:30:00+09:00")
+    const now = new Date(Date.now());
+    //const now = new Date("2024-08-18T00:30:00+09:00");
+    return now;
 }
 
 module.exports = getNow;
